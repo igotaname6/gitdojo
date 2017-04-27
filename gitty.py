70eea05a9af42a92a55fcdd0e9c326a008535951
Szymon Wojtas <szymon.wojtas@gmail.com>
2017-04-27 09:27:38 +0200

    gitting.py added

---
+++ gitty.py
@@ -0,0 +1,16 @@
+
+def adding(x, y):
+    suma = x + y
+    return suma / len(x)
+
+print("Hello, I'm Gittie")
+
+def joke():
+    print("tell joke:")
+print("cos")
+
+print(" kaka ")
+
+def shout():
+    print("shout")
+    print("changed")
